a918591ce689f7e88009a1e46e4439e832e93d7c
ismael <jose.2020110033@metropoli.edu.mx>
2022-12-02 12:12:28 -0600

    Agregar archivos de proyecto.

---
+++ video18/video18.py
@@ -0,0 +1,42 @@
+
+# -*-
+
+coding: utf-8 -t-
+
+Created on Tue May 3 11:43:34 2022
+
+@author: nicosio
+
+class libro:
+
+def _init_(self, paginas): self.paginas paginas
+
+def _add(self, otro):
+
+return self.paginas+otro.paginas
+
+def gt(self,otro):
+
+return self.paginas>otro.paginas
+
+libro1-libro (50)
+
+libro2-libro (300)
+
+if(libro2>libro1):
+
+print('El segundo Libro es mas grande')
+
+else:
+
+print('El primero es mas grande')
+
+#Creamos otra clase que tenga un atributo llamado paginas tambien
+
+class revista: def _init_(self, paginas): self.paginas paginas
+
+class revista: def _init_(self, paginas): self.paginas paginas
+
+revistal-revista (2500)
+
+if libro1>revista1:| print('EL Libro tiene mas paginas') else: print('La revista tiene mas paginas')
